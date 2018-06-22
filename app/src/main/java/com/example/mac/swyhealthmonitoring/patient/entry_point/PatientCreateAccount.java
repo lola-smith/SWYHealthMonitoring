0ad5fb96769ba63a7985500ffa0ee622e9750d06
sondoskhaled <sondoskhaled22@gmail.com>
2018-06-22 20:01:29 +0300

    update patient family

--- app/src/main/java/com/example/mac/swyhealthmonitoring/patient/entry_point/PatientCreateAccount.java
+++ app/src/main/java/com/example/mac/swyhealthmonitoring/patient/entry_point/PatientCreateAccount.java
@@ -1,9 +1,8 @@
 package com.example.mac.swyhealthmonitoring.patient.entry_point;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -12,15 +11,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mac.swyhealthmonitoring.R;
-import com.example.mac.swyhealthmonitoring.doctor.entry_point.DoctorLogin;
-import com.example.mac.swyhealthmonitoring.patient.PatientHome;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-
-import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -110,7 +104,6 @@ public class PatientCreateAccount extends AppCompatActivity {
 
                 Toast.makeText(getApplicationContext(), "User Registred Successfull", Toast.LENGTH_LONG).show();
 
-                startActivity(PatientHome.class, " ");
 
 
             } else {
